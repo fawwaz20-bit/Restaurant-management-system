e1cba42b23d79b3ae06f65228621c7c290b6cd96
Student Name <fawwazahamed2036@gmail.com>
2026-07-28 20:57:08 -0700

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileReader;
+
+public class Main{
+public static void main(String[]args){
+    try {
+        FileReader reader = new FileReader("stud.txt");
+        writer.writer("hello students")
+        }
+        reader.close();
+    }catch(Exception e){
+        System.out.println("Error");
+        }
+    }
+}
